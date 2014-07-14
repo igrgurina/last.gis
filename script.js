3449b9cc08044cc44bfde92ad53be76722361615
Ivan Grgurina <ivan.grgurina@gmail.com>
2014-07-14 11:51:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function Artist(_artistName) {
     this.name = _artistName;
+    this.events = [];
     this.getEvents = function () {
-        e = [];
         $.getJSON("http://ws.audioscrobbler.com/2.0/?method=artist.getevents&artist="
                     + this.name + "&api_key=c7e2dc95d8a8f162ab42118cfb0f30db&format=json",
             function (data) {
                 $.each(data.events.event, function (i, item) {
-                    e.push(new Event(item.title, item.venue.location.city, item.venue.location.country, item.venue.location["get:point"]["geo:lat"], item.venue.location["geo:point"]["geo:long"]));
+                    this.events.push(new Event(item.title, item.venue.location.city, item.venue.location.country, item.venue.location["get:point"]["geo:lat"], item.venue.location["geo:point"]["geo:long"]));
                 });
             }
         );
+        return this.events;
     };
 }
 
@@ -19,9 +20,11 @@ function Event(_title, _city, _country, _lat, _long) {
     this.country = _country;
     this.latitude = _lat;
     this.longitude = _long;
+
     this.getCoordinates = function () {
         return new giscloud.LonLat(this.longitude, this.latitude);
     };
+
     this.getEventMarker = function () {
         return new giscloud.FlagMarker(
             this.getCoordinates().toBounds().center(),
